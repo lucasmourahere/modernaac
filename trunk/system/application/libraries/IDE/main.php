179e9e52c7ea94216a818d1bc404b88cacfbbbe2
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-03-10 18:29:18 +0000

    New method added to IDE Engine which loads injections.

--- trunk/system/application/libraries/IDE/main.php
+++ trunk/system/application/libraries/IDE/main.php
@@ -12,5 +12,14 @@ class IDE{
 		if($_SESSION['logged'] == 1) return true; else return false;
 	}
 	
+	public function loadInjections($name) {
+		if(is_dir("injections/$name"))
+			foreach (glob("injections/$name/*.php") as $injection) {
+				include_once($injection);
+			}
+		else
+			throw new exception("Could not load injections! Event Folder not found. ErrCode: 172610032010");
+	}
+	
 }
 ?>
