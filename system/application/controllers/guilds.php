75334e1a72987b868619fa7aa4c1b5d857e389d6
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-03-27 14:03:07 +0000

    Creating guild functionality.

--- system/application/controllers/guilds.php
+++ system/application/controllers/guilds.php
@@ -23,6 +23,49 @@ class Guilds extends Controller {
 		$this->load->view("view_guild", $data);
 	
 	}
+	
+	public function _checkPlayer($id) {
+		$this->load->model("guilds_model");
+		if($this->guilds_model->checkPlayerCreatingGuild($id)) {
+			return true;
+		}
+		else {
+			$this->form_validation->set_message('_checkPlayer', 'Could not find character.');
+			return false;
+		}
+	}
+	
+	public function _checkGuildName($name){
+		$this->load->model("guilds_model");
+		if($this->guilds_model->checkGuildName($name)) {
+			return true;
+		}
+		else {
+			$this->form_validation->set_message('_checkGuildName', 'Guild name is already taken.');
+			return false;
+		}
+	}
+	
+	public function create() {
+		$ide = new IDE;
+		$ide->requireLogin();
+		$this->load->helper("form");
+		$this->load->model("guilds_model");
+		require_once("system/application/config/create_character.php");
+		$this->load->library("form_validation");
+		if(isset($_POST['submit'])) {
+			$this->form_validation->set_rules('character', 'Character', 'required|numeric|callback__checkPlayer');
+			$this->form_validation->set_rules('name', 'Guild Name', 'required|callback__checkGuildName');
+		}
+		if($this->form_validation->run() == true) {
+			$this->guilds_model->createGuild($_POST['name'], $_POST['character']);
+		}
+		$data = array();
+		$data['characters'] = $this->guilds_model->getCharactersAllowedToCreateGuild($config['levelToCreateGuild']);
+		$data['config'] = $config;
+		$this->load->view("create_guild", $data);
+		
+	}
 
 }
 ?>
