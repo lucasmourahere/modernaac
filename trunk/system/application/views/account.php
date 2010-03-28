876f5f1e0d18f7458815da5e0aec18c10c81deed
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-03-28 20:16:24 +0000

    Fix.

--- trunk/system/application/views/account.php
+++ trunk/system/application/views/account.php
@@ -1,6 +1,7 @@
 <?php 
 	echo "<h2>Hello $loggedUser</h2>";
-	
+	echo "<a href='".WEBSITE."/index.php/character/create_character'>Create character</a><br />";
+	echo "<a href='".WEBSITE."/index.php/account/logout'>Logout</a>";
 	echo "<table width='100%'>";
 	echo "<tr><td><center><b>Name</b></center></td><td><center><b>Level</b></center></td></tr>";
 	foreach($characters as $row) {
