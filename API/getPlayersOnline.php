2806ff5ce0ed824afba4d59e2fbdd1ad50dbdc3a
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-03-29 12:42:00 +0000

    Getting vocation.

--- API/getPlayersOnline.php
+++ API/getPlayersOnline.php
@@ -3,8 +3,8 @@ require("system/system.php");
 auth();
 $db = new database();
 $return = array();
-$sql = $db->query("SELECT `name`, `level`, `world_id` FROM `players` WHERE `online` = '1'");
+$sql = $db->query("SELECT `name`, `level`, `world_id`, `vocation` FROM `players` WHERE `online` = '1'");
 	while($cmd = $sql->fetch_array()) {
-		$return[] = array("name"=>$cmd['name'], "level"=>$cmd['level'], "world_id"=>$cmd['world_id']);
+		$return[] = array("name"=>$cmd['name'], "level"=>$cmd['level'], "world_id"=>$cmd['world_id'], "vocation"=>$cmd['vocation']);
 	}
 echo json_encode($return);
