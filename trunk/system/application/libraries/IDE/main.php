dd5045bc7f916e8b42d34a9fa5dd00db808922b8
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-04-03 14:21:42 +0000

    Rewritten injection method.

--- trunk/system/application/libraries/IDE/main.php
+++ trunk/system/application/libraries/IDE/main.php
@@ -12,10 +12,20 @@ class IDE{
 		if($_SESSION['logged'] == 1) return true; else return false;
 	}
 	
+	public function dir_list($d){ 
+       foreach(array_diff(scandir($d),array('.','..')) as $f)
+			if(is_dir($d.'/'.$f))$l[]=$f; 
+			return $l; 
+	} 
+	
 	public function loadInjections($name) {
-		if(is_dir("injections/$name"))
-			foreach (glob("injections/$name/*.php") as $injection) {
-				include_once($injection);
+		if(is_dir("injections/$name")) {
+				foreach($this->dir_list("injections/$name") as $injection) {
+					if(!file_exists("injections/$name/$injection/injection.php"))
+						continue;
+					else
+						include("injections/$name/$injection/injection.php");
+				}
 			}
 		else
 			throw new exception("Could not load injections! Event Folder not found. ErrCode: 172610032010");
