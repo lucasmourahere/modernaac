c21ff720e3aa35b216fe2d13fb4a75ae1df461e3
rsiersma <rsiersma@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-04-04 19:53:17 +0000

    New config.php

--- trunk/config.php
+++ trunk/config.php
@@ -1,32 +1,33 @@
 <?php
 
 $config = array();
-$config['server_name'] = "Server Name";
+$config['server_name'] = "Test";
 $config['cities'] = array(1=>'Main City');
 $config['vocations'] = array(1=>"Sorcerer", 2=>"Druid", 3=>"Paladin", 4=>"Knight");
-$config['restricted_names'] = array("gamemaster", "admin", "account manager");
-$config['worlds'] = array(0=>"World Name", 1=>"Second World");
-$config['groups'] = array(0=>"Player", 2=>"Tutor", 3=>"Senior Tutor", 4=>"Gamemaster", 5=>"Community Manager", 6=>"God");
+$config['restricted_names'] = array("god", "gamemaster", "admin", "account manager");
+$config['worlds'] = array(0=>"Test");
+$config['groups'] = array(1=>"Player", 2=>"Tutor", 3=>"Senior Tutor", 4=>"Gamemaster", 5=>"Community Manager", 6=>"God");
 $config['server_vocations'] = array(0=>"None", 1=>"Sorcerer", 2=>"Druid", 3=>"Paladin", 4=>"Knight", 5=>"Master Sorcerer", 6=>"Elder Druid", 7=>"Royal Paladin", 8=>"Elite Knight");
-$config['newchar_vocations'][1][0] = "Rook Sample";
-$config['newchar_vocations'][1][1] = "Sorcerer Sample";
-$config['newchar_vocations'][1][2] = "Druid Sample";
-$config['newchar_vocations'][1][3] = "Paladin Sample";
-$config['newchar_vocations'][1][4] = "Knight Sample";
 $config['newchar_vocations'][0][0] = "Rook Sample";
 $config['newchar_vocations'][0][1] = "Sorcerer Sample";
 $config['newchar_vocations'][0][2] = "Druid Sample";
 $config['newchar_vocations'][0][3] = "Paladin Sample";
 $config['newchar_vocations'][0][4] = "Knight Sample";
+$config['newchar_vocations'][1][0] = "Rook Sample";
+$config['newchar_vocations'][1][1] = "Sorcerer Sample";
+$config['newchar_vocations'][1][2] = "Druid Sample";
+$config['newchar_vocations'][1][3] = "Paladin Sample";
+$config['newchar_vocations'][1][4] = "Knight Sample";
 $config['players_group_id_block'] = 3;
 $config['levelToCreateGuild'] = 50;
+$config['latestdeathlimit'] = 20; //Limit of the latest deaths.
 $config['database']['host'] = "localhost";
 $config['database']['login'] = "root";
-$config['database']['password'] = "kubus00";
-$config['database']['database'] = "acc";
+$config['database']['password'] = "XXX";
+$config['database']['database'] = "test";
 $config['layout'] = "default";
-$config['website'] = "http://127.0.0.1/ide_aac";
-$config['title'] = "Modern AAC - Powered by IDE Engine";
+$config['website'] = "http://XXX";
+$config['title'] = "Modern AAC - Powered by Paxton & crew.";
 $config['premDays'] = 30;
 $startPos['x'] = 1000;
 $startPos['y'] = 1000;
