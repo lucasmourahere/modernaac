47efe770aac9d44086ea4364c7c9066dedab8720
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-04-07 14:43:26 +0000

    Added new db.

--- dbSCHEMA.sql
+++ dbSCHEMA.sql
@@ -6,4 +6,13 @@ CREATE TABLE IF NOT EXISTS `news` (
   `body` text NOT NULL,
   `time` int(18) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_id` int(11) DEFAULT NULL,
+  `body` text NOT NULL,
+  `time` int(18) NOT NULL DEFAULT '0',
+  `author` varchar(64) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
