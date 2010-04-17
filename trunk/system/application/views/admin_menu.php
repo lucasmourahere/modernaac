3ecf01e71c5a4854c784b3c1874257eb4579bff2
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-04-17 23:36:03 +0000

    New command feature.

--- trunk/system/application/views/admin_menu.php
+++ trunk/system/application/views/admin_menu.php
@@ -3,5 +3,6 @@ echo "<div class='toolbar'>";
 echo "<a href='".WEBSITE."/index.php/admin/scaffolding'>Scaffolding</a> | ";
 echo "<a href='".WEBSITE."/index.php/admin/news'>News</a> | ";
 echo "<a href='".WEBSITE."/index.php/admin/forum'>Forum</a> | ";
+echo "<a href='".WEBSITE."/index.php/admin/command'>Command</a> | ";
 echo "</div>";
 ?>
