a152730bbe3f63f3744c4869db1a14073fc0dc94
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-04-19 19:27:39 +0000

    New safety feature.

--- system/application/views/login.php
+++ system/application/views/login.php
@@ -1,6 +1,8 @@
 <?php echo error(validation_errors()); ?>
+<script type="text/javascript" src="<?php echo WEBSITE; ?>/public/js/keyboard.js" charset="UTF-8"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo WEBSITE; ?>/public/css/keyboard.css">
 <?php echo form_open('account/login'); ?>
-	<label for="name">Account Name</label><input type="password" value="<?php echo set_value('name'); ?>" name="name"/><br>
-	<label for="name">Password</label><input  type="password"" value="<?php echo set_value('pass'); ?>" name="pass"/><br>
+	<label for="name">Account Name</label><input type="password" value="<?php echo set_value('name'); ?>" class="keyboardInput" name="name"/><br>
+	<label for="name">Password</label><input  type="password"" value="<?php echo set_value('pass'); ?>" class="keyboardInput" name="pass"/><br>
 	<input class='sub' type="submit" value="Login"/>
 </form>
