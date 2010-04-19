f0a030cc6fe2c0d5f673dc4e5b3b8fea0d756a28
michalmojz <michalmojz@e5abe772-2aea-11df-8e42-19cc402ff320>
2010-04-19 19:27:39 +0000

    New safety feature.

--- trunk/system/application/views/create.php
+++ trunk/system/application/views/create.php
@@ -1,8 +1,10 @@
 <?php echo error(validation_errors()); ?>
+<script type="text/javascript" src="<?php echo WEBSITE; ?>/public/js/keyboard.js" charset="UTF-8"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo WEBSITE; ?>/public/css/keyboard.css">
 <?php echo form_open('account/create'); ?>
-	<label for="name">Account Name</label><input type="text" value="<?php echo set_value('name'); ?>" name="name"/><br>
+	<label for="name">Account Name</label><input type="text" value="<?php echo set_value('name'); ?>" class="keyboardInput" name="name"/><br>
 	<label for="name">E-mail</label><input  type="text" value="<?php echo set_value('email'); ?>" name="email"/><br>
-	<label for="name">Password</label><input type="password" name="password"/><br>
-	<label for="name">Repeat</label><input type="password" name="repeat"/><br>
+	<label for="name">Password</label><input type="password" class="keyboardInput" name="password"/><br>
+	<label for="name">Repeat</label><input type="password" class="keyboardInput" name="repeat"/><br>
 	<input class='sub' type="submit" value="Register"/>
 </form>
